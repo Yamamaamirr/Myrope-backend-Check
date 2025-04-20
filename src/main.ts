2fbca103c23a04c3e9ce70ea181d3556764ac8d4
Yamamaamirr <myamama.be21igis@student.nust.edu.pk>
2025-04-21 00:26:09 +0500

    Geofence crud done

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
+  // Global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,7 +18,18 @@ async function bootstrap() {
       transform: true,
     })
   );
-  
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('MyRopes API')
+    .setDescription('The MyRopes API documentation')
+    .setVersion('1.0')
+    .addTag('geofence')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
